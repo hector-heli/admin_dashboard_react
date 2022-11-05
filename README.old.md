9d7645d68a4c0718472873d70e01ade56dd4bbbf
Héctor Helí <hectorheli@misena.edu.co>
2022-11-05 00:37:24 +0000

    Adelanto de tutorial hasta el minuto 21

---
+++ README.old.md
@@ -0,0 +1,5 @@
+# admin_dashboard_react
+
+Este proyecto es el resultado de seguir el tutorial disponible en:
+https://www.youtube.com/watch?v=jx5hdo50a2M 
+
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+        <h1 className='underline text-3xl'> Compromiso - FPI</h1>
+    </div>
+
+  )
+}
+
+export default App
