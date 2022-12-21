8a5cb7ff98698754a47e7893e6947e2bf948cfba
Héctor Helí <hectorheli@misena.edu.co>
2022-12-21 06:26:53 +0000

    Desarrollo de la barra de navegación

--- src/App.js
+++ src/App.js
@@ -1,87 +1,110 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { FiSettings} from 'react-icons/fi';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
-import { Navbar, Footer, Sidebar, ThemeSettings, LineChart } from './components';
-import { Orders, Calendar, Employees, Stacked, Pyramid, Customers, Kanban, Line, Area, Bar, Pie, Financial, ColorPicker, ColorMapping, Editor, ECommerce } from './pages';
+import {
+  Navbar,
+  Footer,
+  Sidebar,
+  ThemeSettings,
+  LineChart,
+} from "./components";
+import {
+  Orders,
+  Calendar,
+  Employees,
+  Stacked,
+  Pyramid,
+  Customers,
+  Kanban,
+  Line,
+  Area,
+  Bar,
+  Pie,
+  Financial,
+  ColorPicker,
+  ColorMapping,
+  Editor,
+  ECommerce,
+} from "./pages";
 
-import { useStateContext } from './context/ContextProvider';
+import { useStateContext } from "./context/ContextProvider";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   const { activeMenu } = useStateContext();
 
   return (
     <div>
-      <BrowserRouter >
-        <div className="flex-relative dark:bg-main-dark-bg">
-          <div className="fixed right-4 bottom-4" style = {{ zIndex: '1000'}} >
-            <TooltipComponent content= "Settings" position='Top'>
-              <button type='button'
-                      className='text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white'
-                      style={{ 
-                        background: 'blue',
-                        borderRadius: '50%' 
-                      }}        >
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-8 bottom-8" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="TopLeft">
+              <button
+                type="button"
+                className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{
+                  background: "blue",
+                  borderRadius: "50%",
+                }}
+              >
                 <FiSettings />
               </button>
             </TooltipComponent>
-
           </div>
-          {activeMenu? (
-            <div className='w-72 fixed sidebar dark:bg-secundary-dark-bg bg-white '> 
+          {activeMenu ? (
+            <div className="w-72 fixed sidebar dark:bg-secundary-dark-bg bg-white ">
               <Sidebar />
             </div>
-          ):(
-            <div className='w-0 dark:bg-secundary-dark-bg'>
+          ) : (
+            <div className="w-0 dark:bg-secundary-dark-bg">
               <Sidebar />
             </div>
           )}
-          <div className={
-            `dark:bg-main-bg  bg-main-bg min-h-screen w-full ${activeMenu ?  'md:ml-72': 'flex-2'}`
-          }>
-            <div className='fixed md:static bg-main-bg dark: bg-main-dark-bg navbar w-full'>
+          <div
+            className={`dark:bg-main-bg  bg-main-bg min-h-screen w-full ${
+              activeMenu ? "md:ml-72" : "flex-2"
+            }`}
+          >
+            <div className="flex-auto md:static bg-main-bg dark:bg-main-dark-bg navbar w-full ">
               <Navbar />
             </div>
+
+            <div>
+              <Routes>
+                {/* Dashboard */}
+                <Route path="/" element={<ECommerce />} />
+                <Route path="/ecommerce" element={<ECommerce />} />
+
+                {/* Pages */}
+                <Route path="/orders" element={<Orders />} />
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/customers" element={<Customers />} />
+
+                {/* Apps */}
+                <Route path="/kanban" element={<Kanban />} />
+                <Route path="/editor" element={<Editor />} />
+                <Route path="/calendar" element={<Calendar />} />
+                <Route path="/color-picker" element={<ColorPicker />} />
+
+                {/* Charts */}
+                <Route path="/line" element={<LineChart />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
+              </Routes>
+            </div>
           </div>
-
-          <div>
-            <Routes>
-              {/* Dashboard */}
-              <Route path='/' element = {< ECommerce/> } />
-              <Route path='/ecommerce' element = {<  ECommerce/> } />
-
-              {/* Pages */}
-              <Route path='/orders' element = {< Orders/> } />
-              <Route path='/employees' element = {< Employees/> } />
-              <Route path='/customers' element = {< Customers/> } />
-
-              {/* Apps */}
-              <Route path='/kanban' element = {< Kanban/> } />
-              <Route path='/editor' element = {< Editor/> } />
-              <Route path='/calendar' element = {< Calendar/> } />
-              <Route path='/color-picker' element = {< ColorPicker/> } />
-              
-              {/* Charts */}
-              <Route path='/line' element = {< LineChart/> } />
-              <Route path='/area' element = {< Area/> } />
-              <Route path='/bar' element = {< Bar/> } />
-              <Route path='/pie' element = {< Pie/> } />
-              <Route path='/financial' element = {< Financial/> } />
-              <Route path='/color-mapping' element = {< ColorMapping/> } />
-              <Route path='/pyramid' element = {< Pyramid />} />
-              <Route path='/stacked' element = {< Stacked/> } />
-
-            </Routes>
-          </div>
-
         </div>
       </BrowserRouter>
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,14 @@ import { useStateContext } from '../context/ContextProvider';
 
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if(activeMenu && screenSize <= 900){
+      setActiveMenu(false);
+    }
+  }
+
   const activeLink = 'flex item-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex item-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
 
@@ -17,14 +24,14 @@ const Sidebar = () => {
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto  pb-10" >
       { activeMenu && (<> 
         <div className='flex justify-between-items-center'>
-          <Link to='/' onClick={() => setActiveMenu(false)} 
+          <Link to='/' onClick={handleCloseSidebar} 
             className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900 ">
             <SiShopify /> 
             <span > Shoppy</span>
           </Link> 
           <TooltipComponent content="Menu" position='BottomCenter'>
             <button type="button" onClick={()=>setActiveMenu((prevActiveMenu) => !prevActiveMenu)} 
-            className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'>
+            className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block xl:hidden'>
             <MdOutlineCancelScheduleSend/>  
             </button>  
           </TooltipComponent> 
@@ -39,7 +46,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => {}}
+                    onClick={handleCloseSidebar}
                     className = {({ isActive }) =>
                     isActive ? activeLink : normalLink }
                   >
